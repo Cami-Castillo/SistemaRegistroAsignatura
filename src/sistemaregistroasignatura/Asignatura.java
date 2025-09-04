cf6503d459a693de60292106b98b6a89cee3eea2
Cami <111029498+Cami-Castillo@users.noreply.github.com>
2025-09-04 19:13:40 -0400

    Update Asignatura.java

--- src/sistemaregistroasignatura/Asignatura.java
+++ src/sistemaregistroasignatura/Asignatura.java
@@ -4,12 +4,12 @@ public class Asignatura {
   private int codigo;
   private String nombreEstudiante;
   private String nombreProfesor;
-  private int nota1;
-  private int nota2;
-  private int nota3;
+  private double nota1;
+  private double nota2;
+  private double nota3;
 
   // Constructor
-  public Asignatura(String nombreAsignatura, int codigo, String nombreEstudiante, String nombreProfesor, int nota1, int nota2, int nota3) {
+  public Asignatura(String nombreAsignatura, int codigo, String nombreEstudiante, String nombreProfesor, double nota1, double nota2, double nota3) {
     this.nombreAsignatura = nombreAsignatura;
     this.codigo = codigo;
     this.nombreEstudiante = nombreEstudiante;
@@ -36,16 +36,16 @@ public class Asignatura {
     return nombreProfesor;
   }
 
-  public int getNota1() {
-    return codigo;
+  public double getNota1() {
+    return nota1;
   }
 
-  public int getNota2() {
-    return codigo;
+  public double getNota2() {
+    return nota2;
   }
 
-  public int getNota3() {
-    return codigo;
+  public double getNota3() {
+    return nota3;
   }
 
   // Setters
@@ -65,15 +65,15 @@ public class Asignatura {
     this.nombreProfesor = nombreProfesor;
   }
 
-  public void setNota1(int nota1) {
+  public void setNota1(double nota1) {
     this.nota1 = nota1;
   }
 
-  public void setNota2(int nota2) {
+  public void setNota2(double nota2) {
     this.nota1 = nota2;
   }
 
-  public void setNota3(int nota3) {
+  public void setNota3(double nota3) {
     this.nota1 = nota3;
   }
 
